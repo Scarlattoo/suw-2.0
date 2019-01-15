d3d8f3e7b0b93a0ee20fd27129d6a5828242d7aa
Róża Żabiłowicz <roza.zabilowicz@tieto.com>
2019-01-15 19:15:34 +0100

    Added correct form to change password - user panel

--- src/AppBundle/Controller/UserPanelController.php
+++ src/AppBundle/Controller/UserPanelController.php
@@ -5,26 +5,32 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Form\ChangePwd;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class UserPanelController extends Controller
 {
-    /**
+     /**
      * @Route("/panel_uzytkownika", name="userPanel")
      */
-    public function indexAction()
+    public function userAction(Request $request)
     {
-        
 
-        return $this->render('userPanel/user.html.twig');
-    }
+        $user = new User();
+        $form = $this->createForm(ChangePwd::class, $user);
+   
+        $form->handleRequest($request);
+   
+        if ($form->isSubmitted() && $form->isValid()) {
 
-      /**
-     * @Route("/panel_uzytkownika/zmiana_hasla", name="change_password")
-     */
-    public function changePassword()
-    {
-        $user=$this->getUser();
-        $files=$this->getDoctrine()->getRepository(User::class);
-        return $this->render('userPanel/user.html.twig');
+            //tu musi byc jeszcze encode i decode
+            
+             return $this->redirect($this->generateUrl('change_passwd_success'));
+         }
+   
+         return $this->render('userPanel/user.html.twig', array(
+             'form' => $form->createView(),
+         ));      
     }
 }
